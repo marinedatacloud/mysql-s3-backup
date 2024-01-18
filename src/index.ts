dc9fcc817ba385eba818f3449a6f86fedc192200
Bledar Haxhia <bledar@marinedatacloud.com>
2024-01-18 22:54:13 +0100

    MDC-2316: Add more logs

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,6 @@ const job = new CronJob(env.BACKUP_CRON_SCHEDULE, async () => {
 
 job.start();
 
-console.log("Backup cron scheduler started.")
+console.log("Backup cron scheduler started.");
+console.log("Current dateTime: "+new Date())
+console.log("BACKUP_CRON_SCHEDULE: "+env.BACKUP_CRON_SCHEDULE)
